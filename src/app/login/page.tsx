b92d55a002dd47a6947ee14d08d67ee7e5eebdd8
Gisselle Burgos <gisselle.burgos.260@my.csun.edu>
2026-04-15 00:09:39 -0700

    add sessionId to localStorage for session tracking on login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,11 +25,11 @@ export default function LoginPage() {
     e.preventDefault();
 
     try {
-      const response = await api.post<{ token: string, user: PublicUser}>("/api/v1/users/login", loginData);
-
+      const response = await api.post<{ token: string, sessionId: string, user: PublicUser}>("/api/v1/users/login", loginData);
       const user: PublicUser = response.data.user;
 
       localStorage.setItem("token", response.data.token);
+      localStorage.setItem("sessionId", response.data.sessionId);
       localStorage.setItem("user", JSON.stringify(response.data.user));
 
       router.push("/dashboard");
